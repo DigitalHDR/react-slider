0951aea085bb305a57f23ce306cfaf7379627622
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-06-13 18:01:00 -0300

    1° parte completo já está tudo funcionado

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const data = {
+  images: [
+    {
+      id:1,
+      imageurl: 'https://i.postimg.cc/fWdKyLG0/firebase.png',
+    },
+    {
+      id:2,
+      imageurl: 'https://i.postimg.cc/fWdKyLG0/firebase.png',
+    },
+    {
+      id:3,
+      imageurl: 'https://i.postimg.cc/fWdKyLG0/firebase.png',
+    },
+    {
+      id:4,
+      imageurl: 'https://i.postimg.cc/fWdKyLG0/firebase.png',
+    },
+    {
+      id:5,
+      imageurl: 'https://i.postimg.cc/fWdKyLG0/firebase.png',
+    },
+  ],
+}
+export default data
